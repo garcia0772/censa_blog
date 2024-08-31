615adf8315c854a370f8cfe0f7e7acdc02303274
garcia0772 <garciamolina0770@gmail.com>
2024-08-31 17:18:47 -0500

    Creacion de listar_post y detalle_post
    Se crea view para listar y ver detalle

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import  agregar_post
+from .views import agregar_post, listar_post, detalle_post
 
 urlpatterns = [
     path('agregar_post/', agregar_post, name='agregar_post'),
+    path('listar_post/', listar_post, name='listar_post'),
+    path('post/<int:pk>/', detalle_post, name='detalle_post'),
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
+from datetime import datetime
 
-from .forms import  PostModelForm
+from .models import Post
+from .forms import PostModelForm
 
 
 # Create your views here.
 def inicio(request):
     pass
     return render(request, 'inicio.html')
+
 
 def agregar_post(request):
     if request.method == 'POST':
@@ -18,3 +22,50 @@ def agregar_post(request):
     else:
         form = PostModelForm
     return render(request, 'agregar_post.html', {'form': form})
+
+
+def listar_post(request):
+    posts_list = Post.objects.all()
+    query = request.GET.get('q')
+    fecha_min_str = request.GET.get('fecha_min')
+    fecha_max_str = request.GET.get('fecha_max')
+
+    try:
+        if fecha_min_str:
+            fecha_min = datetime.strptime(fecha_min_str, '%d/%m/%Y').date()
+        else:
+            fecha_min = None
+
+        if fecha_max_str:
+            fecha_max = datetime.strptime(fecha_max_str, '%d/%m/%Y').date()
+        else:
+            fecha_max = None
+    except ValueError:
+        fecha_min = None
+        fecha_max = None
+
+    if fecha_min and fecha_max:
+        if fecha_min <= fecha_max:
+            posts_list = posts_list.filter(
+                fecha_publicacion__gte=fecha_min,
+                fecha_publicacion__lte=fecha_max
+            )
+        else:
+            posts_list = posts_list.none()
+    elif fecha_min:
+        posts_list = posts_list.filter(fecha_publicacion__gte=fecha_min)
+    elif fecha_max:
+        posts_list = posts_list.filter(fecha_publicacion__lte=fecha_max)
+
+    if query:
+        posts_list = posts_list.filter(Q(titulo__icontains = query) | Q(contenido__icontains = query) | Q(categoria__icontains = query))
+
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    return render(request, 'listar_post.html', {'posts': posts})
+
+def detalle_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'detalle_post.html', {'post': post})
